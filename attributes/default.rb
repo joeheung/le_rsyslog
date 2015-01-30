09828cc6b7236137911f5648afc78a61545f7f11
Joe Heung <joe.heung@logentries.com>
2015-01-30 17:18:33 +0000

    added datahub integration

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,5 +2,9 @@ default['le_rsyslog']['logentries']['logs'] =
     [ {:name => 'alpha', :log => '/var/log/alpha', :token => 'logentries_token'},
       {:name => 'beta', :log => '/var/log/beta', :token => 'logentries_token'} ]
 
-default['le_rsyslog']['logentries']['ssl'] = true
+default['le_rsyslog']['logentries']['ssl'] = false
 default['le_rsyslog']['logentries']['ssl_dir'] = '/opt/ssl'
+
+default['le_rsyslog']['datahub']['enabled'] = false
+default['le_rsyslog']['datahub']['ip'] = '127.0.0.1'
+default['le_rsyslog']['datahub']['port'] = 10000
